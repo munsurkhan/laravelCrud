449f292da0d38cfc61a012b08ea66ac2e810dbcc
Munsur Rahman <munsurinfo@gmail.com>
2023-07-25 23:50:03 +0600

    complete

--- crud/database/migrations/2023_07_25_172408_create_employees_table.php
+++ crud/database/migrations/2023_07_25_172408_create_employees_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('employees', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('avatar');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('employees');
     }
 };
